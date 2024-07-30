04371071f69d5d015bb0b706815070648a9a594f
Vishalkumartarai <vtarai762@gmail.com>
2024-07-30 18:08:15 +0530

    all changes done4

--- client/src/components/YoutubeVideoCard.jsx
+++ client/src/components/YoutubeVideoCard.jsx
@@ -8,7 +8,7 @@ const YouTubeVideoCard = ({ video }) => {
 
   return (
     <div
-      className="relative w-full max-w-[400px] h-[350px] border-4 bg-white border-blue-600 hover:border-blue-800 overflow-hidden rounded-[30px] transition-all duration-300 cursor-pointer mx-auto"
+      className="relative w-full max-w-[400px] h-[350px] border-4 bg-white dark:bg-slate-800 border-white-600 hover:border-blue-600 overflow-hidden rounded-[30px] transition-all duration-300 cursor-pointer mx-auto shadow-lg shadow-blue-500"
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
       onClick={() =>
@@ -16,21 +16,21 @@ const YouTubeVideoCard = ({ video }) => {
       }
     >
       <div
-        className={`w-full h-[60%] flex items-center justify-center p-4 transition-transform duration-300 bg-gray-100 ${
+        className={`w-full h-[60%] flex items-center justify-center p-4 transition-transform duration-300 bg-gray-100 dark:bg-slate-600 ${
           isHovered ? "transform scale-105" : ""
         }`}
       >
         <img
           src={video.coverImage}
           alt={video.heading}
-          className="w-auto h-full object-cover rounded-[20px]"
+          className="w-auto h-full object-cover rounded-[20px] shadow-md  dark:shadow-blue-600"
         />
       </div>
-      <div className="p-4 flex flex-col gap-2 h-[40%] justify-center items-center text-center bg-white">
+      <div className="p-4 flex flex-col gap-2 h-[40%] justify-center items-center text-center bg-white dark:bg-slate-800">
         <p className="text-lg font-semibold line-clamp-2 group-hover:text-blue-800 transition-all duration-300">
           {video.heading}
         </p>
-        <span className="italic text-sm text-gray-700 group-hover:text-blue-800 transition-all duration-300">
+        <span className="italic text-sm text-gray-700 dark:text-gray-300 group-hover:text-blue-800 transition-all duration-300">
           {video.description}
         </span>
       </div>
